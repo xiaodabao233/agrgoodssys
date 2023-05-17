8c18a6fdd41b725ed6c7501db25c93edd73e5281
Chris Xiao <xiaodabao233@163.com>
2023-05-17 12:19:41 +0800

    ssh-keygen -t rsa

---
+++ src/main/java/com/nsu/util/LogUtil.java
@@ -0,0 +1,32 @@
+package com.nsu.util;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.MDC;
+
+public class LogUtil {
+    private static Logger logger = Logger.getLogger(LogUtil.class);
+
+    /**
+     * @param classname 类名
+     * @param method    方法名
+     * @param errorMsg  异常信息
+     */
+    public static void writeLogs(String classname, String method,
+                                 String errorMsg) {
+        try {
+            MDC.put("classname", classname);
+            MDC.put("method", method);
+            if (errorMsg.isEmpty()) {
+                MDC.put("statusCode", 0);
+                logger.info("");
+            } else {
+                MDC.put("statusCode", -1);
+                logger.error(errorMsg);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            MDC.clear();
+        }
+    }
+}
---
+++ src/main/java/com/nsu/vo/UserSignlnForm.java
@@ -0,0 +1,49 @@
+package com.nsu.vo;
+
+public class UserSignlnForm {
+
+    private String userName;
+    private String userPass;
+    private String code;
+
+    //表单里添加验证码
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void UserSignInForm() {
+    }
+
+    public void UserSignInForm(String userName, String userPass) {
+        this.userName = userName;
+        this.userPass = userPass;
+    }
+
+    @Override
+    public String toString() {
+        return "UserSignForm{" +
+                "userName='" + userName + '\'' +
+                ", userPass='" + userPass + '\'' +
+                '}';
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
+    }
+}
